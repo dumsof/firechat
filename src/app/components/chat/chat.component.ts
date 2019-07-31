1afc1ca89b56239cc8ecc5827f42608fd7496fbc
darwinum <dun34@hotmail.com>
2019-07-31 13:14:04 -0500

    se seleccionan los ultimos 5 mensajes, y se ordena

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -14,5 +14,13 @@ export class ChatComponent {
   }
   enviar_mensaje() {
     console.log(this.mensaje);
+    if (this.mensaje.length === 0) {
+      return;
+    }
+    this.sevicioChat.agregarMensaje(this.mensaje)
+      .then(() => { this.mensaje = ''; console.log('Mensaje enviado con exito'); })
+      .catch(error => console.log('Error al guardar :', error));
+    /* el then se ejecuta cuando todo se realiza con exito 
+      catch: se ejecuta cuando hay error*/
   }
 }
--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -15,17 +15,29 @@ export class ChatService {
 
   cargarMensajes() {
 
-    this.itemsCollection = this.afs.collection<Mensaje>('chats');
+    this.itemsCollection = this.afs.collection<Mensaje>('chats', refe => refe.orderBy('fecha', 'desc')
+      .limit(5));
     return this.itemsCollection
       .valueChanges()
       .pipe(
         map((mensajes: Mensaje[]) => {
-          console.log(mensajes);
-          this.chats = mensajes;
+         /*  mostrar ordenado los ultimos 5 mensajes de la colección*/
+          this.chats = [];
+          mensajes.forEach(mensaje => {
+            this.chats.unshift(mensaje);
+          });
+          
+          return this.chats;
         })
       );
   }
   agregarMensaje(texto: string) {
+    let mensaje = {
+      nombre: 'Demo',
+      mensajes: texto,
+      fecha: new Date().getTime()
+    };
 
+    return this.itemsCollection.add(mensaje);
   }
 }
